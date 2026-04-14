9cb86efb837d518a43f08b925ae40448911c8e08
ushanayana <ushanayana@gmail.com>
2026-04-14 22:03:18 +0530

    infra project

--- 06-backend/backend.sh
+++ 06-backend/backend.sh
@@ -1,6 +1,16 @@
-#!/bin/bash
+# #!/bin/bash
+# component=$1
+# environment=$2
+# dnf install ansible -y
+# pip3.9 install botocore boto3
+# ansible-pull -i localhost, -U https://github.com/ushanayana/expense-ansible-roles.git main.yaml -e component=$component -e env=$environment
+
 component=$1
-environment=$2
 dnf install ansible -y
-pip3.9 install botocore boto3
-ansible-pull -i localhost, -U https://github.com/ushanayana/expense-ansible-roles.git main.yaml -e component=$component -e env=$environment
+
+cd /home/ec2-user
+git clone https://github.com/ushanayana/expense-ansible-roles.git
+
+cd expense-ansible-roles
+git pull
+ansible-playbook -e component=$component  main.yaml
