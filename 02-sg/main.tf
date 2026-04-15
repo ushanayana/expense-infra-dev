e3cb8ded0429bb925f5ec52d974dbb2e595a935c
ushanayana <ushanayana@gmail.com>
2026-04-15 19:17:56 +0530

    first commit

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -211,3 +211,13 @@ resource "aws_security_group_rule" "vpn_https" {
   protocol          = "tcp"
   to_port           = 443
 }
+
+resource "aws_security_group_rule" "app_alb_bastion" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id = module.bastion.sg_id # source is where you are getting traffic from
+  security_group_id = module.app_alb.sg_id
+}
+
